efdc8c12ed6f2eb65b669b9680f5a5b4d8fa94e5
WillManAg <willy_fuenla_93@hotmail.com>
2025-09-17 12:32:15 +0200

    Learning boolean

---
+++ src/uwu/booleanos.java
@@ -0,0 +1,28 @@
+package uwu;
+
+public class booleanos {
+
+	public static void main(String[] args) {
+		
+		int i = 3;
+		int numeroEntero1=78,numeroEntero2=90;
+		double numeroReal;
+		char caracter;
+		boolean iguales;
+		
+		iguales=numeroEntero1==numeroEntero2;
+		
+		// estructura condicional simple
+		
+		if (!iguales) {
+			// instrucciones que se ejecutan si la condición es true
+			System.out.println("Son distintos");
+			
+		} else {
+			System.out.println("Son iguales");
+		}
+		
+
+	}
+
+}
