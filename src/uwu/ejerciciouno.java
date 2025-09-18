cc789a2e55278b9359c9ce9dc60843f832ea9c0c
WillManAg <willy_fuenla_93@hotmail.com>
2025-09-18 19:11:02 +0200

    aprendiendo comparativos

---
+++ src/uwu/ejerciciouno.java
@@ -0,0 +1,36 @@
+package uwu;
+
+import java.util.Scanner;
+
+public class ejerciciouno {
+
+	public static void main(String[] args) {
+		
+		double num1, num2;
+		
+		Scanner teclado = new Scanner (System.in);
+		
+		System.out.println("Introduce el número 1");
+		
+		num1 = teclado.nextDouble();
+		
+		System.out.println("Introduce el número 2");
+		
+		num2 = teclado.nextDouble();
+		
+		if (num1 > num2) {
+			
+			System.out.println(num1 + " es mayor que " + num2);
+			
+		} else if (num1 < num2) {
+			
+			System.out.println(num1 + " es menor que " + num2);
+		} else {
+			
+			System.out.println(num1 + " y " + num2 + " son iguales.");
+		}
+		
+		teclado.close();
+	}
+
+}
