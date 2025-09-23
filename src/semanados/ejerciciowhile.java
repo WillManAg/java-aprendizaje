16c38648d04df0ff9a7bf1f8c4b3bf27e169d079
WillManAg <willy_fuenla_93@hotmail.com>
2025-09-23 11:00:01 +0200

    Utilizando while

---
+++ src/semanados/ejerciciowhile.java
@@ -0,0 +1,33 @@
+package semanados;
+
+public class ejerciciowhile {
+
+	public static void main(String[] args) {
+		
+		// Hacer que cuente los numeros del 1 al 10
+		
+		int numero = 1;
+		
+		while(numero <= 10) {
+			
+			System.out.println(numero);
+			
+			numero++;
+			
+		}
+		
+		// Hacer que cuente los pasos del 10 al 1
+		
+		int otronum = 10;
+		
+		while(otronum >= 1) {
+			
+			System.out.println(otronum);
+			
+			otronum--;
+			
+		}
+
+	}
+
+}
---
+++ src/semanados/empezandowhile.java
@@ -0,0 +1,20 @@
+package semanados;
+
+public class empezandowhile {
+
+	public static void main(String[] args) {
+		
+		int numero = 1;
+		
+		while(numero <= 10) {
+			
+			System.out.println("Hola");
+			
+			// Le suma +1 al numero de la variable, así detenemos el bucle
+			numero++;
+			
+		}
+
+	}
+
+}
