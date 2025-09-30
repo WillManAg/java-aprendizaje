5bbd8acd14a4780073a117f79d1a8ec1c7d11fe2
WillManAg <willy_fuenla_93@hotmail.com>
2025-09-30 19:26:24 +0200

    Ejercicio 41 Derrepente todo es más bonito y fácil

--- src/semanatres/ejercicio41.java
+++ src/semanatres/ejercicio41.java
@@ -3,8 +3,17 @@ package semanatres;
 public class ejercicio41 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		final int numMax = 700;
+		int total = 0;
+		
+		for (int i=10; i <= numMax ; i+=2) {
+			
+			total += i;
+		}
+
+		System.out.println("La suma de todos los múltiplos de 2 desde el 10 hasta el 700 es: " + total);
 	}
 
 }
