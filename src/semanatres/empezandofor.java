b31252857c6c2a1278c00a7b5d522d028c05c906
WillManAg <willy_fuenla_93@hotmail.com>
2025-09-30 09:06:08 +0200

    Practicas simples con bucles for

--- src/semanatres/empezandofor.java
+++ src/semanatres/empezandofor.java
@@ -3,13 +3,31 @@ package semanatres;
 public class empezandofor {
 
 	public static void main(String[] args) {
+		
+		// creciente
 
 		for (int i=1; i<11; i++) {
 			
-			System.out.println("Contar hasta " + i);
+			System.out.print(i + " ");
+		}
+
+		System.out.println();
+		// decreciente
+		
+		for (int i=10; i>0; i--) {
+			
+			System.out.print(i + " ");
 		}
 		
-
+		System.out.println();
+		// pares
+		
+		for (int i=2; i<11; i+=2) {
+			
+			System.out.print(i + " ");
+		}
+		
+		
 	}
 
 }
