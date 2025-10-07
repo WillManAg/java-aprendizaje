907744e278b3e1fd008f07ed4f274edb6da37e44
WillManAg <willy_fuenla_93@hotmail.com>
2025-10-07 12:20:15 +0200

    Revisando la posición en memoria del punto y lso rectangulos

---
+++ objetosBici/src/rectangulo/Main.java
@@ -0,0 +1,22 @@
+package rectangulo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Point punto1;
+		Rectangle rectangulo1, rectangulo2;
+		
+		// llamamos al constructor
+		// le pasamos el punto
+		rectangulo1 = new Rectangle(new Point (3,4),6,7);
+		punto1 = new Point(5,6);
+		rectangulo2 = new Rectangle(punto1,6,7);
+		
+		System.out.println(rectangulo1);
+		System.out.println(rectangulo2);
+		System.out.println(punto1);
+
+	}
+
+}
