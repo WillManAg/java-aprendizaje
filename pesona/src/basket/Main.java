0a3201373a47d7558a61f2e166e5bd1b657d974e
WillManAg <willy_fuenla_93@hotmail.com>
2025-10-14 08:51:26 +0200

    Creando el baloncesto

--- pesona/src/basket/Main.java
+++ pesona/src/basket/Main.java
@@ -3,8 +3,40 @@ package basket;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Jugador jugador1, jugador2;
+		
+		jugador1 = new Jugador();
+		jugador2 = new Jugador(2.07, 98,1);
+	
+		
+		if (jugador1.lanzarDeDos()) {
+			System.out.println("J1 de 2: Canasta");
+		} else {
+			System.out.println("J1 de 2: Falla");
+		}
+		
+		if (jugador2.lanzarDeDos()) {
+			System.out.println("J2 de 2: Canasta");
+		} else {
+			System.out.println("J2 de 2: Falla");
+		}
 
+
+		if (jugador1.lanzarDeTres()) {
+			System.out.println("J1 de 3: Canasta");
+		} else {
+			System.out.println("J1 de 3: Falla");
+		}
+		
+		if (jugador2.lanzarDeTres()) {
+			System.out.println("J2 de 3: Canasta");
+		} else {
+			System.out.println("J2 de 3: Falla");
+		}
+
+		
+		
 	}
 
 }
