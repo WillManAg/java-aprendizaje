37378c237a1d2e730d2eecc076409ef03390a899
WillManAg <willy_fuenla_93@hotmail.com>
2025-10-16 14:01:16 +0200

    Nuevo ejercicio de clases

---
+++ pesona/src/fechas/Fecha.java
@@ -0,0 +1,97 @@
+package fechas;
+
+public class Fecha {
+	
+	private int dia = 1, mes = 1, ano = 1900;
+
+	public Fecha(){
+		
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+	
+	public Fecha(int dia, int mes, int ano){
+		
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+		
+	}
+	
+	public boolean esBisiesto() {
+		
+		if ((this.ano % 4 == 0 && this.ano % 100 != 0) || (this.ano % 400 == 0)) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	public int diaMes() {
+		
+		int dias = 0;
+		
+		switch(this.mes) {
+		case 1: dias = 31;
+		break;
+		case 2: if (esBisiesto()) {
+            dias = 29;
+        } else {
+            dias = 28;
+        }
+        break;
+		case 3: dias = 31;
+		break;
+		case 4: dias = 30;
+		break;
+		case 5: dias = 31;
+		break;
+		case 6: dias = 30;
+		break;
+		case 7: dias = 31;
+		break;
+		case 8: dias = 31;
+		break;
+		case 9: dias = 30;
+		break;
+		case 10: dias = 31;
+		break;
+		case 11: dias = 30;
+		break;
+		case 12: dias = 31;
+		break;
+		}
+		return dias;
+	}
+
+	
+	private void valida() {
+		
+		if (!(this.dia >= 1 && this.dia <= 31)) {
+			this.dia = 1;
+		}
+		
+		if (!(this.mes >= 1 && this.mes <= 12)) {
+			this.mes = 1;
+		}
+		
+		if (!(this.ano >= 1900 && this.ano <= 2050)) {
+			this.ano = 1900;
+		}
+		
+	}
+	
+	public void siguiente() {
+		this.dia+=1;
+		// hay que tener en cuenta que si es 31, tienes que pasar al siguiente mes no el dia
+	}
+	
+	public void anterior() {
+		this.dia-=1;
+	}
+	
+	
+	
+}
