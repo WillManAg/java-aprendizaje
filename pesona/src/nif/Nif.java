b46a6b249b81966e4f248981283a4cc9f73a030b
WillManAg <willy_fuenla_93@hotmail.com>
2025-10-16 12:46:59 +0200

    Seguimos metiendo caña a la programacion orientada a objetos

---
+++ pesona/src/nif/Nif.java
@@ -0,0 +1,33 @@
+package nif;
+
+public class Nif {
+	
+	private int numero;
+	private char letra;
+	private final int DIVISOR_LETRAS = 23;
+
+	public Nif() {
+		this.numero = 0;
+		this.letra = ' ';
+	}
+	
+	public Nif(int numero) {
+		this.numero = numero;
+		this.asignarLetra();
+	}
+	
+	private void asignarletra() {
+		
+		switch(this.numero%this.DIVISOR_LETRAS) {
+			case 0: this.letra= 'T';
+			break;
+			case 1: this.letra='R';
+			break;
+				// resto de letras hata el 22
+			
+			
+		}
+		
+	}
+	
+}
